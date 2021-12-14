806c93f9bceae8c69a0b7f5c44a5fce1208a1533
Mehedi Hasan <hasan.mehedi@bjitgroup.com>
2021-12-15 02:07:04 +0600

    design a simple navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,21 @@
-import {BrowserRouter, Route, Switch} from "react-router-dom";
-import {Home, Login, Register} from "./pages";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Home, Login, Register } from "./pages";
+import { Nav } from "./components";
 
 export const App = () => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path='/home' component={Home}/>
-        <Route exact path='/login' component={Login}/>
-        <Route exact path='/register' component={Register}/>
-      </Switch>
+      <Nav />
+
+      <div className="container" style={{ marginTop: "2%" }}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </Switch>
+      </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
