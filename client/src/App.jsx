706a458494952b581a4c13e7c77ac7944ee70a2d
Mehedi Hasan <hasan.mehedi@bjitgroup.com>
2021-12-14 23:45:27 +0600

    implemented react router functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {Home, Login, Register} from "./pages";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/home' component={Home}/>
+        <Route exact path='/login' component={Login}/>
+        <Route exact path='/register' component={Register}/>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
